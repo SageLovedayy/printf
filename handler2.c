39b56b019f95c40949994eee774348db6a71135d
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-16 21:32:00 +0100

    Ad

--- handler2.c
+++ handler2.c
@@ -36,10 +36,29 @@ int handle_pointer(va_list args, struct FormatSettings *formatSettings)
 	buffer[buffer_len++] = 'x';
 	buffer[buffer_len++] = '0';
 	/* Write the hexadecimal string in reverse order */
-	for (i = buffer_len - 1; i >= 0; i--)
+	while (buffer_len < formatSettings->precision)
 	{
-		write(1, &buffer[i], 1);
+		buffer[buffer_len++] = '0';
 	}
 
-	return (buffer_len);
+	// Apply width and flags
+	if (formatSettings->flags & 1) {  // Left-justify flag '-'
+		for (i = 0; i < buffer_len; i++) {
+			write(1, &buffer[i], 1);
+		}
+		while (buffer_len < formatSettings->width) {
+			write(1, " ", 1);
+			buffer_len++;
+		}
+	} else {
+		while (buffer_len < formatSettings->width) {
+			write(1, " ", 1);
+			buffer_len++;
+		}
+		for (i = buffer_len - 1; i >= 0; i--) {
+			write(1, &buffer[i], 1);
+		}
+	}
+
+	return buffer_len;
 }
