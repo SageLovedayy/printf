b06686a2f712ebb41d29c29da480eb8e3200024b
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-16 21:35:47 +0100

    Fix

--- handler2.c
+++ handler2.c
@@ -36,29 +36,10 @@ int handle_pointer(va_list args, struct FormatSettings *formatSettings)
 	buffer[buffer_len++] = 'x';
 	buffer[buffer_len++] = '0';
 	/* Write the hexadecimal string in reverse order */
-	while (buffer_len < formatSettings->precision)
+	for (i = buffer_len - 1; i >= 0; i--)
 	{
-		buffer[buffer_len++] = '0';
+		write(1, &buffer[i], 1);
 	}
 
-	// Apply width and flags
-	if (formatSettings->flags & 1) {  // Left-justify flag '-'
-		for (i = 0; i < buffer_len; i++) {
-			write(1, &buffer[i], 1);
-		}
-		while (buffer_len < formatSettings->width) {
-			write(1, " ", 1);
-			buffer_len++;
-		}
-	} else {
-		while (buffer_len < formatSettings->width) {
-			write(1, " ", 1);
-			buffer_len++;
-		}
-		for (i = buffer_len - 1; i >= 0; i--) {
-			write(1, &buffer[i], 1);
-		}
-	}
-
-	return buffer_len;
+	return (buffer_len);
 }
